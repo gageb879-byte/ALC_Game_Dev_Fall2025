67df600b8f03f1a5efbbc3b9a5efc85884728567
gageb879-byte <gage.b879@stu.nebo.edu>
2025-09-05 13:55:38 -0600

    Balloon POP

--- Prototype  1 - Tank Game 2025/Assets/Scripts/PlayerController.cs
+++ Prototype  1 - Tank Game 2025/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public float rotSpeed;
     public float hInput;
     public float vInput;
+    public float jInput;
     public float jumpForce;
     
     public Rigidbody playerRB;
@@ -17,9 +18,11 @@ public class PlayerController : MonoBehaviour
     {
         hInput = Input.GetAxis("Horizontal");
         vInput = Input.GetAxis("Vertical");
+        jInput = Input.GetAxis("Jump");
 
         transform.Rotate(Vector3.up, hInput * rotSpeed * Time.deltaTime); //Rotates the vehicle left and right (side-to-side)
-        transform.Translate(Vector3.forward * vInput * speed *Time.deltaTime); // Moves the vehicle forward and backward        
+        transform.Translate(Vector3.forward * vInput * speed *Time.deltaTime); // Moves the vehicle forward and backward  
+        transform.Translate(Vector3.up * jInput * jumpForce * Time.deltaTime); // Lets vehicle jump up    
     }
 
 }
