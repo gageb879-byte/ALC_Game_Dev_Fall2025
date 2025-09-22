4d6d294088e0b343b737cfde462acbb94cdeb48e
gageb879-byte <gage.b879@stu.nebo.edu>
2025-09-22 13:37:18 -0600

    Edited spawn speeds and fixed effects from acting weird

--- Prototype  1 - Tank Game 2025/Prototype 2 - Balloon POP Game/Assets/Scripts/Moveup.cs
+++ Prototype  1 - Tank Game 2025/Prototype 2 - Balloon POP Game/Assets/Scripts/Moveup.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class Moveup : MonoBehaviour
 {
         public float speed;
+        public float upperBound = 15.0f; // Set an upper bound for balloons to pass and for penalties to be applied
+        public ScoreManager scoreManager; // Referencing the Score Manager
+         public Balloon balloon;// Reference the balloon script to get the score
 
+        
         // Start is called before the first frame update
     void Start()
     {
-
+        scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+        balloon = GetComponent<Balloon>();
     }
 
 
@@ -17,5 +22,11 @@ public class Moveup : MonoBehaviour
     {
         // Move the balloon up on the y axis at a set rate of speed
         transform.Translate(Vector3.up * speed );
+
+        if(transform.position.y >= upperBound)
+        {
+            scoreManager.DecreaseScoreText(balloon.scoreToGive); // Reduce score for allowing the balloon to leave the screen
+            Destroy(gameObject); //pop the ballon after it leaves the screen
+        }
     }
 }
