5e9e7553dc9c3674557a9759f3ef0a4e65d6ceb5
gageb879-byte <gage.b879@stu.nebo.edu>
2025-10-29 14:15:52 -0600

    Made stuff actually work

--- Prototype  1 - Tank Game 2025/2D Platformer/Assets/Scripts/Enemy.cs
+++ Prototype  1 - Tank Game 2025/2D Platformer/Assets/Scripts/Enemy.cs
@@ -43,7 +43,7 @@ public class Enemy : MonoBehaviour
 
     }
     
-    private void onTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         
         if(collision.CompareTag("Player"))
